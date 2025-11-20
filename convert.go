c6e2c97150b11b6a42c4ee52de4ebf84c3a110a9
vaelixd <vaelixd@proton.me>
2025-11-20 16:22:28 +1300

    implement all conversion functions

--- convert.go
+++ convert.go
@@ -1,10 +1,65 @@
 package scsv
 
-func AsCSV() {
+import "fmt"
+
+func AsCSV(parsedSCSV KeyValuePairs) string {
+	conversion := ""
+
+	for key, values := range parsedSCSV {
+		for _, value := range values {
+			conversion += fmt.Sprintf("%s,%s\n", key, value)
+		}
+	}
+
+	return conversion
 }
 
-func AsJSON() {
+func AsJSON(parsedSCSV KeyValuePairs) string {
+	conversion := "{\n"
+	keyIdx := 0
+
+	for key, values := range parsedSCSV {
+		keyIdx++
+
+		arrayDelim := ",\n"
+		if keyIdx == len(parsedSCSV) {
+			arrayDelim = "\n"
+		}
+		conversion += fmt.Sprintf("  \"%s\": [\n", key)
+
+		for i, value := range values {
+			delim := ",\n"
+			if i == len(values)-1 {
+				delim = "\n"
+			}
+			conversion += fmt.Sprintf("    \"%s\"%s", value, delim)
+		}
+
+		conversion += fmt.Sprintf("  ]%s", arrayDelim)
+	}
+
+	conversion += "}\n"
+
+	return conversion
 }
 
-func AsYAML() {
+func AsYAML(parsedSCSV KeyValuePairs) string {
+	conversion := ""
+
+	for key, values := range parsedSCSV {
+		conversion += fmt.Sprintf("%s:", key)
+
+		if len(values) <= 1 {
+			conversion += " []\n"
+			continue
+		} else {
+			conversion += "\n"
+		}
+
+		for _, value := range values {
+			conversion += fmt.Sprintf("  - %s\n", value)
+		}
+	}
+
+	return conversion
 }
